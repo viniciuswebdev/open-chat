5b6deed951660742eb200b9cad82ec4d1bfc96ae
vinicius <viniciuswebdev@gmail.com>
2013-09-29 12:25:53 -0300

    Add home page styles

--- public/script.js
+++ public/script.js
@@ -1,29 +1,43 @@
 var socket = io.connect();
+var UserName;
+
+$(function() {
+	setFakeName();
+	$("#nameSet").click(function() {setUsetName()});
+	$("#submit").click(function() {sentMessage();});
+});
+
+function setFakeName(){
+	var fakeUserName = "User" + Math.floor((Math.random()*100)+1);
+	socket.emit('setUsetName', fakeUserName);
+	UserName = fakeUserName;
+}
+
 function addMessage(msg, pseudo) {
 	$("#entries").append('<div class="message"><p>' + pseudo + ' : ' + msg + '</p></div>');
 }
+
 function sentMessage() {
 	if ($('#messageInput').val() != "") 
 	{
 		socket.emit('message', $('#messageInput').val());
-		addMessage($('#messageInput').val(), "Me", new Date().toISOString(), true);
+		addMessage($('#messageInput').val(), UserName, new Date().toISOString(), true);
 		$('#messageInput').val('');
 	}
 }
+
 function setUsetName() {
 	if ($("#pseudoInput").val() != "")
 	{
-		socket.emit('setUsetName', $("#pseudoInput").val());
+		var realUserName = $("#pseudoInput").val();
+		socket.emit('setUsetName', realUserName);
+		UserName = realUserName;
 		$('#controll').show();
 		$('#pseudoInput').hide();
 		$('#nameSet').hide();
 	}
 }
+
 socket.on('message', function(data) {
 	addMessage(data['message'], data['pseudo']);
-});
-$(function() {
-	$("#controll").hide();
-	$("#nameSet").click(function() {setUsetName()});
-	$("#submit").click(function() {sentMessage();});
 });
