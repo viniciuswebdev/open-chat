b53d51503324e11642ee471997a18e4da2abbe08
vinicius <viniciuswebdev@gmail.com>
2013-09-29 02:02:47 -0300

    Add support for rooms

--- app.js
+++ app.js
@@ -29,13 +29,14 @@ var MessageSchema = new Schema({
 var Message = mongoose.model("Message", MessageSchema);
 
 io.sockets.on('connection', function (socket) {
+	socket.join('room1');
 	socket.on('setUsetName', function (data) {
 		socket.set('pseudo', data);
 	});
 	socket.on('message', function (message) {
 		socket.get('pseudo', function (error, name) {
 			var data = { 'message' : message, pseudo : name };
-			socket.broadcast.emit('message', data);
+			socket.broadcast.to('room1').emit('message', data);
 			console.log("user " + name + " send this : " + message);
 		})
 	});
