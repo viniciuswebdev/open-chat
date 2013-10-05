f6900f068368a8458eb3d6cea5021a1e7fd19123
vinicius <viniciuswebdev@gmail.com>
2013-10-05 19:32:30 -0300

    User model must export a object with methods instead functions

--- socket/index.js
+++ socket/index.js
@@ -1,30 +1,31 @@
 var express = require('express'),
 	parseCookie = express.cookieParser(),
-    db = require('../db');
+    User = require('../model/user');
+	userModel = new User;
 
 exports.connection = function (socket) {
 	socket.on('disconnect', function () {
-		db.updateToOut(socket.handshake.sessionId)
+		userModel.updateToOut(socket.handshake.sessionId)
 		socket.broadcast.emit('deleteUser', socket.id);
     });
 
 	socket.on('connect', function () {
-		db.getUser(socket.handshake.sessionId, function(user){
+		userModel.getUser(socket.handshake.sessionId, function(user){
 			if(!user){
-				db.insertUser(socket.id, socket.handshake.sessionId, function(user){
+				userModel.insertUser(socket.id, socket.handshake.sessionId, function(user){
 					socket.set('name', user.name);
 					socket.broadcast.emit('addNewUser', user);
-					db.getAllUsers(function(users){
+					userModel.getAllUsers(function(users){
 						socket.emit('addUserList', {users:users, socket:user.socket});
 					});
 				});				
 			}else{
 				if(socket.id != user.socket){
-					db.updateUserSocket(socket.id, user.session, function(newuser){
+					userModel.updateUserSocket(socket.id, user.session, function(newuser){
 						socket.broadcast.emit('deleteUser', user.socket);
 						socket.set('name', newuser.name);
 						socket.broadcast.emit('addNewUser', newuser);
-						db.getAllUsers(function(users){
+						userModel.getAllUsers(function(users){
 							socket.emit('addUserList', {users:users, socket:newuser.socket});
 						});
 					});
