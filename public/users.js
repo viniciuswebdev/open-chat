e0a70544f63f130a8f8a3ef70cc7ffe290998927
vinicius <viniciuswebdev@gmail.com>
2013-10-25 23:27:19 -0200

    Implement model encapsulation

--- public/users.js
+++ public/users.js
@@ -1,23 +1,23 @@
-Users = {
+function Users () {
 
-	userList : {},
-	userName: {},
+	this.userList = {};
+	userName = {};
 
-	listenUserSettings: function() {
+	this.listenUserSettings = function() {
 		$(document).on('click','.userme', function(){
 			$(".userme").popover(); 
 		});
-	},
+	};
 
-	setUserName: function (userName) {
+	this.setUserName = function (userName) {
 		this.userName = userName;
-	},
+	};
 
-	getUserName: function () {
+	this.getUserName = function () {
 		return this.userName;
-	},
+	};
 
-	listenUserChangeName: function() {
+	this.listenUserChangeName = function() {
 		$(document).on('keypress','#userChangeName', function(e){
 			var code = (e.keyCode ? e.keyCode : e.which);
 			if(code == 13) {
@@ -27,9 +27,9 @@ Users = {
 				Socket.changeUserName(Users.getUserName());
 			}
 		});
-	},
+	};
 
-	createSelfUserDom: function (userName, socket) {
+	this.createSelfUserDom = function (userName, socket) {
 		var userNameDiv = document.createElement("div");
 		userNameDiv.className = "userme";
 		userNameDiv.id = socket;
@@ -44,9 +44,9 @@ Users = {
 		nameTitle.appendChild(name);
 
 		return userNameDiv;
-	},
+	};
 
-	createNewUserDom: function (userName, socket) {
+	this.createNewUserDom = function (userName, socket) {
 		var userNameDiv = document.createElement("div");
 		userNameDiv.className = "user";
 		userNameDiv.id = socket;
@@ -60,35 +60,35 @@ Users = {
 		nameTitle.appendChild(name);
 
 		return userNameDiv;
-	},
+	};
 
-	addUser: function (userName, socket) {	
+	this.addUser = function (userName, socket) {	
 		$(this.createNewUserDom(userName, socket)).appendTo(this.userList).hide().fadeIn(1000);
-	},
+	};
 
-	deleteUser: function (socket) {
+	this.deleteUser = function (socket) {
     	$("#" + socket).fadeOut(1000, function() { 
     		$(this).remove(); 
     	});
-	},
+	};
 
-	enableUserSettings: function() {
+	this.enableUserSettings = function() {
 		$(".userme").popover({
 			html:true,
 		 	content: $("#userSettings").html(),
 			title: "Settings"
 		});
-	},
+	};
 
-	addSelfUser: function (userName, socket) {
+	this.addSelfUser = function (userName, socket) {
 		$(this.createSelfUserDom(userName, socket)).prependTo(this.userList).hide().fadeIn(1000);
 		this.enableUserSettings();
-	},
+	};
 
-	init: function() {
+	this.init = function() {
 		this.userList = $("#contact-list");
 		this.listenUserSettings();
 		this.listenUserChangeName();
-	}
+	};
 
 };
